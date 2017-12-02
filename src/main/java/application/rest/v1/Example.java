1b3689feb5628c3bb8984bf1781c44326ecf8888
ibmdemo <ibmdemo@gmail.com>
2017-12-01 17:46:40 -0800

    restore

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -20,7 +20,7 @@ public class Example {
     public Response example() {
         List<String> list = new ArrayList<>();
         //return a simple list of strings
-        list.add("Congratulations, your application is up and running 201712");
+        list.add("Congratulations, your application is up and running");
         return Response.ok(list.toString()).build();
     }
 
